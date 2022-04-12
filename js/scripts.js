d20355c28335c0f9fd1986be0fc08980f7bb91c9
Naaman <naamanh10@gmail.com>
2022-04-12 14:26:51 +0000

    changes and projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 var myCarousel = document.querySelector('#homeBanner')
 var carousel = new bootstrap.Carousel(myCarousel)
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+document.getElementById("copyUrl").addEventListener("click", function(event){
+    event.preventDefault();
+    var url = window.location.href;
+    var utm = "?utm_source=site&utm_medium=share";
+    navigator.clipboard.writeText(url + utm);
+    console.log(event);
+
+});
+
